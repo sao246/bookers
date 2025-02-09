f6bf6bcd5beeb253a3c984849b68274e22b5d809
sao246 <195246331+sao246@users.noreply.github.com>
2025-02-09 11:54:16 +0000

    [finish] 3-12 index画面の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,31 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
 
   def show
-    @books = Book.find(params[:id]) 
+    @book = Book.find(params[:id]) 
   end
 
   def edit
-    @books = Book.find(params[:id]) 
+    @book = Book.find(params[:id]) 
   end
 
   def index
-  
+    @book = Book.new
+    @books = Book.all
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#new'
+  get 'books' => 'books#index'
+   get 'books' => 'books#new'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
   root :to => 'homes#top'
-  resources :lists
+  resources :lists 
+ 
+  get 'books/:id' => 'books#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
