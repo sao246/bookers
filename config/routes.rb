a1fc205d3aaf42e3a367d9412eeece5270a97124
sao246 <195246331+sao246@users.noreply.github.com>
2025-02-09 06:43:49 +0000

    [finish] 3-12 top画面、Startリンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/' => 'books#new'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' =>'books#edit'
+  get 'books' => 'books#new' as: 'book'
+  get 'books/:id' => 'books#show' as: 'show_book'
+  get 'books/:id/edit' =>'books#edit' as: 'edit_book'
   root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
