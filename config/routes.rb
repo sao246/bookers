2a9b607a01bd7c46b5cb919e57fc131e576b270f
sao246 <195246331+sao246@users.noreply.github.com>
2025-02-09 07:34:12 +0000

    [finish] 3-12 routesファイルの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'books' => 'books#new'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' =>'books#edit'
   root :to => 'homes#top'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
