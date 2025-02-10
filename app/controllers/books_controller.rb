5b93e519fc89bf59f0cf642d8da89a15e697d89c
sao246 <195246331+sao246@users.noreply.github.com>
2025-02-10 11:53:06 +0000

    [finish] 3-12 Update時のバリデーションチェック

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,10 +28,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)  
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  get 'books' => 'books#new'
-  post 'books' => 'books#create'
-  
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  get 'books/:id/' => 'books#show', as: 'book'
   root :to => 'homes#top'
-  get 'books/:id' => 'books#show'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
