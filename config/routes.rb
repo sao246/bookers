2ab34bf1595375267a6a8ce79b753e50f7b7d0f4
sao246 <195246331+sao246@users.noreply.github.com>
2025-02-10 10:12:22 +0000

    [finish] 3-12 削除機能、flashメッセージ、validation追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
-  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/' => 'books#show', as: 'book'
   root :to => 'homes#top'
   get 'books/:id' => 'books#show'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
