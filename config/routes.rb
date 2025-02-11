0af69f06fd13451a7311c0f3c3db79e623d6fefa
sao246 <195246331+sao246@users.noreply.github.com>
2025-02-11 03:05:17 +0000

    [finish] 3-12 テスト実施・編集画面のバグ修正・作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
