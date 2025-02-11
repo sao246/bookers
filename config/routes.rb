6ed2880eedd7e6e8d96f11f82ed53a5d6470e8f8
sao246 <195246331+sao246@users.noreply.github.com>
2025-02-11 04:08:14 +0000

    [finish] 3-12 課題提出前final

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
